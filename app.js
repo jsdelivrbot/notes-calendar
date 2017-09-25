91486995177ad7e6e563b651c3c4b90729d873bc
alex <sokolovskiialex@gmail.com>
2017-09-25 18:00:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 'use strict';
 
-//var express = require('express');
-//var app = express();
-//var config = require('./src/config/express.json');
-//var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var config = require('./src/config/express.json');
+var bodyParser = require('body-parser');
 // "postinstall": "npm install express",
 // heroku logs --app notes-calendar
 // for parsing application/json
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 // for parsing application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({
-  //  extended: true
-//}));
-//app.use(express.static(__dirname + config.rootDirectory));
-// require('./src/middlewares/custom-render')(app);
-// require('./src/routes/index')(app);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + config.rootDirectory));
+//require('./src/middlewares/custom-render')(app);
+//require('./src/routes/index')(app);
 
-//app.get('/', function(request, response) {
-  //response.render('index.html');
-//});
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
 
-//Error 404
-//app.use(function(req, res) {
-  //  res.onRejected({
-    //    code: 404,
-      //  message: 'Not found!'
-    //});
-//});
-var express = require('express');
+Error 404
+app.use(function(req, res) {
+    res.onRejected({
+        code: 404,
+        message: 'Not found!'
+    });
+});
+
+app.listen(config.port, function() {
+    console.log('Server start on port: ' + (process.env.PORT || config.port));
+});
+/*var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -40,7 +44,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-//app.listen(config.port, function() {
-    //console.log('Server start on port: ' + (process.env.PORT || config.port));
-//});
+});*/
